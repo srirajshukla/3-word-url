dd0351c2c84c580f488f03ab82a64fa7ae085fd1
Gyanendra Shukla <srirajshukla@gmail.com>
2020-12-05 20:01:08 +0530

    Added function to check the validity of url, and raise exceptions appropriately.
    Signed-off-by: srirajshukla <srirajshukla@gmail.com>

--- api/purifyurl.py
+++ api/purifyurl.py
@@ -1,10 +1,25 @@
 from urllib.parse import urlparse
 
+import requests
+
+
+def testurl(url):
+    try:
+        url = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        raise
+    if url.ok:
+        return True
+    else:
+        print("here")
+        url.raise_for_status()
+
 
 def purifyurl(url):
     urlobj = urlparse(url)
-    if urlobj.scheme == '':
-        urlobj._replace(scheme='http')
-    elif urlobj.scheme == 'https':
-        urlobj._replace(scheme='http')
-    return urlobj.geturl()
+    if testurl(url):
+        if urlobj.scheme == '':
+            urlobj._replace(scheme='http')
+        elif urlobj.scheme == 'https':
+            urlobj._replace(scheme='http')
+        return urlobj.geturl()
