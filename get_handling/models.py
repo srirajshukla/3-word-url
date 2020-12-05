db78758f4fa31179ad48b974fbebce6dbd394bb8
Gyanendra Shukla <srirajshukla@gmail.com>
2020-12-05 22:57:17 +0530

    added models for url duration, and some class metadata
    Signed-off-by: srirajshukla <srirajshukla@gmail.com>

--- get_handling/models.py
+++ get_handling/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 import api.shortener
 
@@ -7,4 +8,26 @@ import api.shortener
 
 class long_and_short(models.Model):
     long_url = models.CharField(max_length=500)
-    shortform = models.CharField(max_length=30, null=True, editable=False, default=api.shortener.short(long_url))
+    shortform = models.CharField(max_length=30, null=True, default=api.shortener.short(long_url))
+
+    class DurationHours(models.TextChoices):
+        ONE_HOUR = "1", _("One Hr")
+        THREE_HOUR = "3", _("Three Hrs")
+        SIX_HOUR = "6", _("Six Hrs")
+        TWENTRYFOUR_HOUR = "24", _("Twentyfour Hrs")
+        FOREVER = "0", _("Forever")
+
+    duration = models.CharField(
+        max_length=2,
+        choices=DurationHours.choices,
+        default=DurationHours.FOREVER
+    )
+
+    created = models.DateTimeField(auto_now_add=True)
+    created.editable = True
+
+    def __str__(self):
+        return self.long_url
+
+    def __repr__(self):
+        return f'Long: {self.long_url}\nShort: {self.shortform}\nCreated on: {self.created}\nDuration: {self.duration}'
