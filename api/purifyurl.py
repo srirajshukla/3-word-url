b552ad7ca712620d9cfe0680e11007a3a4f7cfe1
Gyanendra Shukla <srirajshukla@gmail.com>
2020-12-05 19:14:58 +0530

    Improved url purification method.
    Signed-off-by: srirajshukla <srirajshukla@gmail.com>

--- api/purifyurl.py
+++ api/purifyurl.py
@@ -3,10 +3,8 @@ from urllib.parse import urlparse
 
 def purifyurl(url):
     urlobj = urlparse(url)
-    redirecturl = ""
     if urlobj.scheme == '':
-        redirecturl += "http://"
-    else:
-        redirecturl += urlobj.scheme + "://"
-    redirecturl += urlobj.netloc + urlobj.path
-    return redirecturl
+        urlobj._replace(scheme='http')
+    elif urlobj.scheme == 'https':
+        urlobj._replace(scheme='http')
+    return urlobj.geturl()
